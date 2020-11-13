730be1c94a0ee5d65c3f48bf89bd2282dbd78b48
Conrad Bekondo <philllittle302@gmail.com>
2020-11-13 19:36:42 +0100

    database configured

--- Pollsar.Web.Server/Startup.cs
+++ Pollsar.Web.Server/Startup.cs
@@ -10,7 +10,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+using Pollsar.Web.Data;
+using Pollsar.Web.Server.Models;
+
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+
+using Microsoft.AspNetCore.ResponseCompression;
 
 namespace Pollsar.Web.Server
 {
@@ -32,6 +44,25 @@ namespace Pollsar.Web.Server
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pollsar.Web.Server", Version = "v1" });
             });
+            services.AddDbContext<PollsarContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<User, IdentityRole<long>>(options => {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequiredLength = 6;
+            })
+                .AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<PollsarContext>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +79,7 @@ namespace Pollsar.Web.Server
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
